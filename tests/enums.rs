a5ecd44417b0d50ec4054bf4203067213f674659
James Irwin <irwineffect@gmail.com>
2021-02-16 08:34:35 -0800

    ran 'cargo fmt'

--- tests/enums.rs
+++ tests/enums.rs
@@ -1,22 +1,19 @@
 use miniconf::{Error, StringSet};
 use serde::Deserialize;
 
-
-
 #[test]
 fn simple_enum() {
-
     #[derive(StringSet, Debug, Deserialize, PartialEq)]
     enum Variant {
         A,
         B,
-    }    
+    }
 
     #[derive(StringSet, Debug, Deserialize)]
     struct S {
-        v: Variant
+        v: Variant,
     }
-    
+
     let mut s = S { v: Variant::A };
 
     let field = "v".split('/').peekable();
