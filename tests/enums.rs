a453f19d6a5c2668194010db93e2c1950162bf84
James Irwin <irwineffect@gmail.com>
2021-02-16 08:33:28 -0800

    added simple unit test for enum

---
+++ tests/enums.rs
@@ -0,0 +1,27 @@
+use miniconf::{Error, StringSet};
+use serde::Deserialize;
+
+
+
+#[test]
+fn simple_enum() {
+
+    #[derive(StringSet, Debug, Deserialize, PartialEq)]
+    enum Variant {
+        A,
+        B,
+    }    
+
+    #[derive(StringSet, Debug, Deserialize)]
+    struct S {
+        v: Variant
+    }
+    
+    let mut s = S { v: Variant::A };
+
+    let field = "v".split('/').peekable();
+
+    s.string_set(field, "\"B\"".as_bytes()).unwrap();
+
+    assert_eq!(s.v, Variant::B);
+}
