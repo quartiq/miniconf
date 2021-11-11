413a5e3306c031a0ed7eed6b13643970750a8869
James Irwin <irwineffect@gmail.com>
2021-11-11 15:55:16 -0800

    added alternative method for iteration

--- examples/readback.rs
+++ examples/readback.rs
@@ -37,10 +37,11 @@ pub struct SettingsMiniconfIter<'a, 'b, const TS: usize, const VS: usize> {
 // This will eventually be derived
 impl<'a, const TS: usize, const VS: usize> Iterator for SettingsMiniconfIter<'a, '_, TS, VS>{
     type Item = (String<TS>, String<VS>);
-    fn next(&mut self) -> Option<(String<TS>, String<VS>)> {
+    fn next(&mut self) -> Option<Self::Item> {
         let mut topic_buffer: String<TS> = String::new();
         let mut value_buffer: String<VS> = String::new();
         topic_buffer.clear();
+        value_buffer.clear();
         if let Some(()) = self.settings.recursive_iter::<TS, VS>(&mut self.index, &mut topic_buffer, &mut value_buffer) {
             Some((topic_buffer, value_buffer))
         }
@@ -49,6 +50,38 @@ impl<'a, const TS: usize, const VS: usize> Iterator for SettingsMiniconfIter<'a,
         }
     }
 }
+
+// Possible alternative to implementing Iterator?
+pub struct IterState<T: Miniconf+Serialize, const S: usize> {
+    index: [usize; S],
+    _type: core::marker::PhantomData<T>,
+}
+
+impl<'a, T: Miniconf+Serialize, const S: usize> IterState<T,S> {
+    fn new() -> IterState<T,S> {
+        IterState {
+            index: [0; S],
+            _type: core::marker::PhantomData::<T>,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.index = [0; S];
+    }
+
+    fn next<const TS: usize, const VS: usize>(&mut self, settings: &T, topic_buffer: &'a mut String<TS>, value_buffer: &'a mut String<VS>)
+        -> Option<(&'a String<TS>, &'a String<VS>)> {
+        topic_buffer.clear();
+        value_buffer.clear();
+        if let Some(()) = settings.recursive_iter::<TS, VS>(&mut self.index, topic_buffer,  value_buffer) {
+            Some((topic_buffer, value_buffer))
+        }
+        else {
+            None
+        }
+    }
+}
+
 
 fn main() {
     let mut s = Settings {
@@ -82,4 +115,26 @@ fn main() {
     for (topic, value) in settings_iter {
         println!("{} {}", topic, value);
     }
+
+
+    println!("\nAlternative Implementation:");
+    let mut value_buffer: String<128> = String::new();
+    let mut topic_buffer: String<128> = String::new();
+
+    let mut iter_state: IterState<Settings, 5> = IterState::new();
+
+    loop {
+        if let Some((topic, value)) = iter_state.next(&s, &mut topic_buffer, &mut value_buffer) {
+            println!("{} {}", topic, value);
+        }
+        else {
+            // done iterating, break out
+            break;
+        }
+
+        // Proving that settings can be modified between iteration calls
+        s.data = s.data.wrapping_add(1);
+    }
+
+
 }
