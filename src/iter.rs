04d4cea8010e70591939e99fda527f6aa9a4479c
Robert Jördens <rj@quartiq.de>
2022-11-01 08:42:01 +0100

    iter: rename generics a bit

--- src/iter.rs
+++ src/iter.rs
@@ -1,12 +1,12 @@
 use super::Miniconf;
 use heapless::String;
 
-pub struct MiniconfIter<'a, Settings: Miniconf + ?Sized, const TS: usize> {
-    pub(crate) settings: &'a Settings,
+pub struct MiniconfIter<'a, M: ?Sized, const TS: usize> {
+    pub(crate) settings: &'a M,
     pub(crate) state: &'a mut [usize],
 }
 
-impl<'a, Settings: Miniconf + ?Sized, const TS: usize> Iterator for MiniconfIter<'a, Settings, TS> {
+impl<'a, M: Miniconf + ?Sized, const TS: usize> Iterator for MiniconfIter<'a, M, TS> {
     type Item = String<TS>;
 
     fn next(&mut self) -> Option<Self::Item> {
