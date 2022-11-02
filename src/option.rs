d3e6da4bdcad0e5a5db78f5279169384ce743fd9
Robert Jördens <rj@quartiq.de>
2022-11-02 14:04:06 +0100

    remove redundant comment

--- src/option.rs
+++ src/option.rs
@@ -121,7 +121,6 @@ impl<T: crate::Serialize + crate::DeserializeOwned> Miniconf for core::option::O
             return Err(Error::PathTooLong);
         }
 
-        // Note(unwrap): checked above
         let data = self.as_ref().ok_or(Error::PathAbsent)?;
         serde_json_core::to_slice(data, value).map_err(|_| Error::SerializationFailed)
     }
