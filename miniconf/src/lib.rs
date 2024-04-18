cea32b33a54711f413aa0e4b3a81e874aef1dc9a
Robert Jördens <rj@quartiq.de>
2024-04-18 17:03:37 +0200

    default doc

--- miniconf/src/lib.rs
+++ miniconf/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(any(test, doctest, feature = "std")), no_std)]
 #![cfg_attr(all(feature = "json-core", feature = "postcard"), doc = include_str!("../../README.md"))]
-#![cfg_attr(not(feature = "json-core"), doc = "Miniconf")]
+#![cfg_attr(not(all(feature = "json-core", feature = "postcard")), doc = "Miniconf")]
 #![deny(rust_2018_compatibility)]
 #![deny(rust_2018_idioms)]
 #![warn(missing_docs)]
