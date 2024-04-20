59c24959a025933c94804731b5832785b60ca16f
Robert Jördens <rj@quartiq.de>
2024-04-20 22:14:44 +0200

    back to y-1 PhantomData

--- miniconf/src/phantom.rs
+++ miniconf/src/phantom.rs
@@ -1,28 +1,33 @@
 use crate::{Error, Keys, Metadata, TreeKey};
 use core::marker::PhantomData;
 
-impl<T: TreeKey<Y>, const Y: usize> TreeKey<Y> for PhantomData<T> {
-    #[inline]
-    fn len() -> usize {
-        T::len()
-    }
+macro_rules! depth {
+    ($($y:literal)+) => {$(
+        impl<T: TreeKey<{$y - 1}>> TreeKey<$y> for PhantomData<T> {
+            #[inline]
+            fn len() -> usize {
+                0
+            }
 
-    #[inline]
-    fn name_to_index(value: &str) -> Option<usize> {
-        T::name_to_index(value)
-    }
+            #[inline]
+            fn name_to_index(_value: &str) -> Option<usize> {
+                None
+            }
 
-    #[inline]
-    fn traverse_by_key<K, F, E>(keys: K, func: F) -> Result<usize, Error<E>>
-    where
-        K: Keys,
-        F: FnMut(usize, &str, usize) -> Result<(), E>,
-    {
-        T::traverse_by_key(keys, func)
-    }
+            #[inline]
+            fn traverse_by_key<K, F, E>(keys: K, func: F) -> Result<usize, Error<E>>
+            where
+                K: Keys,
+                F: FnMut(usize, &str, usize) -> Result<(), E>,
+            {
+                T::traverse_by_key(keys, func)
+            }
 
-    #[inline]
-    fn metadata() -> Metadata {
-        T::metadata()
-    }
+            #[inline]
+            fn metadata() -> Metadata {
+                T::metadata()
+            }
+        }
+    )+}
 }
+depth!(2 3 4 5 6 7 8);
