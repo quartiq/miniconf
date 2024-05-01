9700ea177d7aecfa46ba0e1c6009a12f7eea2d4f
Robert Jördens <rj@quartiq.de>
2024-05-01 23:01:39 +0200

    impl std smart pointers

--- miniconf/examples/reflect.rs
+++ miniconf/examples/reflect.rs
@@ -1,45 +1,52 @@
 use core::any::{Any, TypeId};
 use miniconf::{JsonPath, TreeAny, TreeKey};
+#[cfg(feature = "std")]
+use std::{rc::Rc, sync::Arc};
 
-struct Caster<T: ?Sized + Any> {
-    cast_ref: fn(from: &dyn Any) -> &T,
-    cast_mut: fn(from: &mut dyn Any) -> &mut T,
-    // std: Box<T>, Rc<T>, Arc<T>
+struct Caster<T: ?Sized> {
+    ref_: fn(from: &dyn Any) -> Option<&T>,
+    mut_: fn(from: &mut dyn Any) -> Option<&mut T>,
+    #[cfg(feature = "std")]
+    box_: fn(from: Box<dyn Any>) -> Result<Box<T>, Box<dyn Any>>,
+    #[cfg(feature = "std")]
+    rc: fn(from: Rc<dyn Any>) -> Result<Rc<T>, Rc<dyn Any>>,
+    #[cfg(feature = "std")]
+    arc: fn(from: Arc<dyn Any + Sync + Send>) -> Result<Arc<T>, Arc<dyn Any + Sync + Send>>,
 }
 
 macro_rules! casters {
     ($trait:path: $ty:ty) => {(
         TypeId::of::<$ty>(),
         &Caster::<dyn $trait> {
-            cast_ref: |from| from.downcast_ref::<$ty>().unwrap(),
-            cast_mut: |from| from.downcast_mut::<$ty>().unwrap(),
-        }
+            ref_: |any| any.downcast_ref::<$ty>().map(|t| t as _),
+            mut_: |any| any.downcast_mut::<$ty>().map(|t| t as _),
+            #[cfg(feature = "std")]
+            box_: |any| any.downcast::<$ty>().map(|t| t as _),
+            #[cfg(feature = "std")]
+            rc: |any| any.downcast::<$ty>().map(|t| t as _),
+            #[cfg(feature = "std")]
+            arc: |any| Err(any), // any.downcast::<$ty>().map(|t| t as _),
+        },
     )};
-    ($trait:path => $($ty:ty),+) => {(
+    ($trait:path => $( $ty:ty ),+ ) => {(
         TypeId::of::<dyn $trait>(),
-        &[ $(casters!($trait: $ty)),+ ]
+        &[ $( casters!($trait: $ty) ),+ ]
     )};
 }
 
 pub struct Registry<'a> {
     // use a suitable hashmap (maybe phf+linkme when TypeId::of() is const),
     // boxes, vecs, lazy caster creation, and a static where possible
-    kv: &'a [(TypeId, &'a [(TypeId, &'a dyn Any)])],
+    casters: &'a [(TypeId, &'a [(TypeId, &'a dyn Any)])],
 }
 
 impl<'a> Registry<'a> {
-    fn caster<'b, T: ?Sized + 'static>(&self, any: &'b dyn Any) -> Option<&Caster<T>> {
-        let id = TypeId::of::<T>();
-        self.kv
-            .iter()
-            .find(|(trait_id, _)| trait_id == &id)
-            .and_then(|(_, types)| {
-                let id = (&*any).type_id();
-                types
-                    .iter()
-                    .find(|(type_id, _)| type_id == &id)
-                    .map(|(_, caster)| caster.downcast_ref().unwrap())
-            })
+    fn caster<'b, T: ?Sized + 'static>(&self, any: &dyn Any) -> Option<&Caster<T>> {
+        let target = TypeId::of::<T>();
+        let (_, types) = self.casters.iter().find(|(trait_id, _)| trait_id == &target)?;
+        let source = (&*any).type_id();
+        let (_, caster) = types.iter().find(|(type_id, _)| type_id == &source)?;
+        caster.downcast_ref()
     }
 
     pub fn implements<T: ?Sized + 'static>(&self, any: &dyn Any) -> bool {
@@ -47,11 +54,46 @@ impl<'a> Registry<'a> {
     }
 
     pub fn cast_ref<'b, T: ?Sized + 'static>(&self, any: &'b dyn Any) -> Option<&'b T> {
-        self.caster(any).map(|caster| (caster.cast_ref)(any))
+        let caster = self.caster(any)?;
+        (caster.ref_)(any)
     }
 
     pub fn cast_mut<'b, T: ?Sized + 'static>(&self, any: &'b mut dyn Any) -> Option<&'b mut T> {
-        self.caster(any).map(|caster| (caster.cast_mut)(any))
+        let caster = self.caster(any)?;
+        (caster.mut_)(any)
+    }
+
+    #[cfg(feature = "std")]
+    pub fn cast_box<'b, T: ?Sized + 'static>(
+        &self,
+        any: Box<dyn Any>,
+    ) -> Result<Box<T>, Box<dyn Any>> {
+        if let Some(c) = self.caster(&any) {
+            (c.box_)(any)
+        } else {
+            Err(any)
+        }
+    }
+
+    #[cfg(feature = "std")]
+    pub fn cast_rc<'b, T: ?Sized + 'static>(&self, any: Rc<dyn Any>) -> Result<Rc<T>, Rc<dyn Any>> {
+        if let Some(c) = self.caster(&any) {
+            (c.rc)(any)
+        } else {
+            Err(any)
+        }
+    }
+
+    #[cfg(feature = "std")]
+    pub fn cast_arc<'b, T: ?Sized + 'static>(
+        &self,
+        any: Arc<dyn Any + Sync + Send>,
+    ) -> Result<Arc<T>, Arc<dyn Any + Sync + Send>> {
+        if let Some(c) = self.caster(&any) {
+            (c.arc)(any)
+        } else {
+            Err(any)
+        }
     }
 }
 
@@ -70,9 +112,9 @@ struct Settings {
 fn main() {
     use core::fmt::{Debug, Display, Formatter, Write};
     let registry = Registry {
-        kv: &[
-            casters!(Display => u8, i32, String),
-            casters!(Debug => u8, i32, String, &[u8]),
+        casters: &[
+            casters!(Display => u8, i32, String, &str),
+            casters!(Debug => u8, i32, String, &[u8], &str),
             casters!(Write => String, Formatter),
             // casters!(erased_serde::Serialize => ...),
             // casters!(erased_serde::Deerialize => ...),
