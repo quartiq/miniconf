4c7b49be89b39d0a83cf032e0910c253b497ee74
Robert Jördens <rj@quartiq.de>
2024-05-05 16:40:49 +0200

    crosstrait: comments

--- crosstrait/tests/embedded/src/main.rs
+++ crosstrait/tests/embedded/src/main.rs
@@ -15,6 +15,21 @@ use core::ops::{AddAssign, SubAssign};
 register! { i32 => dyn AddAssign<i32> }
 register! { u32 => dyn SubAssign<u32> }
 
+#[derive(Default, Tree)]
+struct Inner {
+    val: i32,
+}
+
+#[derive(Default, Tree)]
+struct Settings {
+    #[tree(depth = 1)]
+    a: [i32; 2],
+    #[tree(depth = 2)]
+    i: [Inner; 3],
+    #[tree(depth = 1)]
+    b: Option<i32>,
+}
+
 #[entry]
 fn main() -> ! {
     assert_eq!(crosstrait::REGISTRY_KV.len(), 2);
@@ -24,29 +39,19 @@ fn main() -> ! {
     );
 
     let mut a = 3i32;
-    let v: &mut dyn AddAssign<i32> = (&mut a as &mut dyn Any).cast().unwrap();
-    *v += 5;
+    let any: &mut dyn Any = &mut a;
+
+    let val: &mut dyn AddAssign<i32> = any.cast().unwrap();
+    *val += 5;
     assert_eq!(a, 3 + 5);
 
-    #[derive(Default, Tree)]
-    struct Inner {
-        val: i32,
-    }
-
-    #[derive(Default, Tree)]
-    struct Settings {
-        #[tree(depth = 1)]
-        a: [i32; 2],
-        #[tree(depth = 2)]
-        i: [Inner; 3],
-        #[tree(depth = 1)]
-        b: Option<i32>,
-    }
     let mut s = Settings::default();
     s.i[1].val = 3;
-    let key = miniconf::Packed::new_from_lsb(0b1_01_01_0).unwrap();
-    // let key = miniconf::JsonPath::from(".i[1].val");
+
+    // let key = miniconf::Packed::new_from_lsb(0b1_01_01_0).unwrap();
+    let key = miniconf::JsonPath::from(".i[1].val");
     let any = s.mut_any_by_key(key).unwrap();
+
     let val: &mut dyn AddAssign<i32> = any.cast().unwrap();
     *val += 5;
     assert_eq!(s.i[1].val, 3 + 5);
