d6eb70bdd77fb0bfa4ff42b07b24b28386938001
Robert Jördens <rj@quartiq.de>
2024-06-27 12:26:34 +0200

    cli: dump, reset, simplification

--- miniconf_cli/src/lib.rs
+++ miniconf_cli/src/lib.rs
@@ -1,26 +1,43 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use core::fmt::{self, Display, Write};
+use core::fmt::{self};
 use core::marker::PhantomData;
 
-use embedded_io_async::{Read as IoRead, Write as IoWrite};
+use embedded_io::Write;
+use embedded_io_async::Write as AWrite;
 use heapless::String;
 
-use miniconf::{JsonCoreSlash, PathIter, Traversal, TreeKey};
+use miniconf::{JsonCoreSlash, Keys, Packed, PackedIter, PathIter, Postcard, Traversal, TreeKey};
+
+struct WriteWrap<T>(T);
+
+impl<T: Write> fmt::Write for WriteWrap<T> {
+    fn write_char(&mut self, c: char) -> fmt::Result {
+        let mut buf = [0; 4];
+        self.write_str(c.encode_utf8(&mut buf))
+    }
+
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.0.write_all(s.as_bytes()).or(Err(fmt::Error))?;
+        Ok(())
+    }
+
+    fn write_fmt(&mut self, args: fmt::Arguments<'_>) -> fmt::Result {
+        self.0.write_fmt(args).or(Err(fmt::Error))?;
+        Ok(())
+    }
+}
+
+async fn awrite<W: AWrite>(mut write: W, buf: &[u8]) -> Result<(), Error<W::Error>> {
+    write.write_all(buf).await.map_err(Error::Io)
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Error<I> {
-    Fmt(fmt::Error),
+    Fmt(core::fmt::Error),
     Traversal(miniconf::Traversal),
     Serialize(serde_json_core::ser::Error),
-    Deserialize(serde_json_core::de::Error),
     Io(I),
-}
-
-impl<I> From<fmt::Error> for Error<I> {
-    fn from(value: fmt::Error) -> Self {
-        Self::Fmt(value)
-    }
 }
 
 impl<I> From<Traversal> for Error<I> {
@@ -40,169 +57,211 @@ impl<I> From<miniconf::Error<serde_json_core::ser::Error>> for Error<I> {
     }
 }
 
-impl<I> From<miniconf::Error<serde_json_core::de::Error>> for Error<I> {
-    fn from(value: miniconf::Error<serde_json_core::de::Error>) -> Self {
-        match value {
-            miniconf::Error::Inner(_depth, e) => Self::Deserialize(e),
-            miniconf::Error::Traversal(e) => Self::Traversal(e),
-            miniconf::Error::Finalization(e) => Self::Deserialize(e),
-            _ => unimplemented!(),
-        }
+impl<I> From<usize> for Error<I> {
+    fn from(value: usize) -> Self {
+        Self::Traversal(Traversal::TooLong(value))
     }
 }
 
-impl<I: Display> Display for Error<I> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Fmt(_) => write!(f, "Path buffer: too short"),
-            Self::Traversal(e) => write!(f, "Traversal: {e}"),
-            Self::Io(e) => write!(f, "IO: {e}"),
-            Self::Deserialize(e) => write!(f, "Deserialization: {e}"),
-            Self::Serialize(e) => write!(f, "Serialization: {e}"),
-            _ => unreachable!(),
+impl<I> From<miniconf::Error<fmt::Error>> for Error<I> {
+    fn from(value: miniconf::Error<fmt::Error>) -> Self {
+        match value {
+            miniconf::Error::Inner(_depth, e) => Self::Fmt(e),
+            miniconf::Error::Traversal(e) => Self::Traversal(e),
+            miniconf::Error::Finalization(_) => unreachable!(),
+            _ => unimplemented!(),
         }
     }
 }
 
 pub const SEPARATOR: &str = "/";
 
-#[derive(Debug, PartialEq, PartialOrd, Default)]
-pub struct Menu<M, const Y: usize, const S: usize>
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct Menu<M, const Y: usize, const D: usize = Y>
 where
-    M: TreeKey<Y> + 'static,
+    M: TreeKey<Y> + ?Sized,
 {
-    key: String<S>,
+    key: Packed,
     _m: PhantomData<M>,
 }
 
-impl<M, const Y: usize, const S: usize> Menu<M, Y, S>
+impl<M, const Y: usize, const D: usize> Default for Menu<M, Y, D>
 where
-    M: TreeKey<Y>,
+    M: TreeKey<Y> + ?Sized,
 {
-    fn append(&self, path: Option<&str>) -> Result<String<S>, fmt::Error> {
-        let mut key = self.key.clone();
-        if let Some(path) = path {
-            key.write_str(SEPARATOR)?;
-            key.write_str(path)?;
-        }
-        Ok(key)
-    }
-
-    pub fn enter(&mut self, path: &str) -> Result<(), Error<()>> {
-        let key = self.append(Some(path))?;
-        let mut it = key.split(SEPARATOR).skip(1);
-        let (_idx, depth) = M::indices(&mut it)?;
-        if it.next().is_some() {
-            return Err(Error::Traversal(miniconf::Traversal::TooLong(depth)));
-        }
-        self.key = key;
-        Ok(())
-    }
-
-    pub fn exit(&mut self) -> Result<(), Error<()>> {
-        if let Some(pos) = self.key.rfind(SEPARATOR) {
-            self.key.truncate(pos);
-            Ok(())
-        } else {
-            Err(Error::Traversal(miniconf::Traversal::TooShort(0)))
-        }
-    }
-
-    pub fn list<const D: usize, E>(
-        &self,
-        path: Option<&str>,
-    ) -> Result<impl Iterator<Item = Result<String<S>, fmt::Error>>, Error<E>> {
-        let mut iter = PathIter::<M, Y, String<S>, D>::new(SEPARATOR);
-        let key = self.append(path)?;
-        let mut it = key.split(SEPARATOR).skip(1);
-        let (idx, depth) = M::indices(&mut it)?;
-        if it.next().is_some() {
-            return Err(Error::Traversal(miniconf::Traversal::TooLong(depth)));
-        }
-        iter.root(idx[..depth].iter().copied())?;
-        Ok(iter)
-    }
-
-    pub fn get<E>(
-        &self,
-        instance: &M,
-        path: Option<&str>,
-        buf: &mut [u8],
-    ) -> Result<usize, Error<E>>
-    where
-        M: for<'de> JsonCoreSlash<'de, Y>,
-    {
-        Ok(instance.get_json(&self.append(path)?, buf)?)
-    }
-
-    pub fn set<E>(
-        &mut self,
-        instance: &mut M,
-        path: Option<&str>,
-        buf: &[u8],
-    ) -> Result<usize, Error<E>>
-    where
-        M: for<'de> JsonCoreSlash<'de, Y>,
-    {
-        Ok(instance.set_json(&self.append(path)?, buf)?)
-    }
-
-    pub async fn dump<W, const D: usize, const B: usize>(
-        &self,
-        instance: &M,
-        path: Option<&str>,
-        mut write: W,
-    ) -> Result<usize, Error<W::Error>>
-    where
-        W: IoWrite,
-        M: for<'de> JsonCoreSlash<'de, Y> + Default,
-    {
-        let mut buf = [0; B];
-        let def = M::default();
-        let mut len = 0;
-        len += awrite(&mut write, self.key.as_bytes()).await?;
-        len += awrite(&mut write, "\n".as_bytes()).await?;
-        for keys in self.list::<D, _>(path)? {
-            let keys = keys?;
-            let ret = match instance.get_json(&keys, &mut buf) {
-                Err(miniconf::Error::Traversal(Traversal::Absent(_) | Traversal::TooShort(_))) => {
-                    continue;
-                }
-                ret => &buf[..ret?],
-            };
-            let check: u32 = yafnv::fnv1a(ret.iter().copied());
-            len += awrite(&mut write, "  ".as_bytes()).await?;
-            len += awrite(&mut write, &keys.as_bytes()[self.key.len()..]).await?;
-            len += awrite(&mut write, ": ".as_bytes()).await?;
-            len += awrite(&mut write, ret).await?;
-            let ret = match def.get_json(&keys, &mut buf) {
-                Err(miniconf::Error::Traversal(Traversal::Absent(_depth))) => "absent".as_bytes(),
-                ret => &buf[..ret?],
-            };
-            if yafnv::fnv1a::<u32, _>(ret.iter().copied()) == check {
-                len += awrite(&mut write, " (default)\n".as_bytes()).await?;
-            } else {
-                len += awrite(&mut write, " (default: ".as_bytes()).await?;
-                len += awrite(&mut write, ret).await?;
-                len += awrite(&mut write, ")\n".as_bytes()).await?;
-            }
-        }
-        Ok(len)
+    fn default() -> Self {
+        Self::new(Packed::default())
     }
 }
 
-async fn awrite<W: IoWrite>(mut write: W, buf: &[u8]) -> Result<usize, Error<W::Error>> {
-    write
-        .write_all(buf)
-        .await
-        .map_err(Error::Io)
-        .and(Ok(buf.len()))
+impl<M, const Y: usize, const D: usize> Menu<M, Y, D>
+where
+    M: TreeKey<Y> + ?Sized,
+{
+    pub fn new(key: Packed) -> Self {
+        Self {
+            key,
+            _m: PhantomData,
+        }
+    }
+
+    fn push(&self, path: &str) -> Result<(Self, usize), miniconf::Error<()>> {
+        let mut new = self.key.chain(path.split(SEPARATOR));
+        let (key, depth) = M::packed(new.keys_ref())?;
+        if !new.is_empty() {
+            Err(miniconf::Traversal::TooLong(depth).into())
+        } else {
+            Ok((Self::new(key), depth))
+        }
+    }
+
+    fn pop(&self, levels: usize) -> Result<(Self, usize), miniconf::Error<()>> {
+        let (idx, depth) = M::indices(self.key)?;
+        if depth < levels {
+            Err(miniconf::Traversal::TooShort(depth).into())
+        } else {
+            let (key, depth) = M::packed(idx[..depth - levels].iter().copied())?;
+            Ok((Self::new(key), depth))
+        }
+    }
+
+    pub fn enter(&mut self, path: &str) -> Result<usize, miniconf::Error<()>> {
+        let (new, depth) = self.push(path)?;
+        *self = new;
+        Ok(depth)
+    }
+
+    pub fn exit(&mut self, levels: usize) -> Result<usize, miniconf::Error<()>> {
+        let (new, depth) = self.pop(levels)?;
+        *self = new;
+        Ok(depth)
+    }
+
+    pub fn list<const S: usize>(
+        &self,
+    ) -> Result<impl Iterator<Item = Result<String<S>, fmt::Error>>, Traversal> {
+        let mut iter = PathIter::<M, Y, String<S>, D>::new(SEPARATOR);
+        iter.root(self.key)?;
+        Ok(iter)
+    }
+
+    pub fn get(
+        &self,
+        instance: &M,
+        buf: &mut [u8],
+    ) -> Result<usize, miniconf::Error<serde_json_core::ser::Error>>
+    where
+        M: for<'de> JsonCoreSlash<'de, Y>,
+    {
+        instance.get_json_by_key(self.key, buf)
+    }
+
+    pub fn set(
+        &mut self,
+        instance: &mut M,
+        buf: &[u8],
+    ) -> Result<usize, miniconf::Error<serde_json_core::de::Error>>
+    where
+        M: for<'de> JsonCoreSlash<'de, Y>,
+    {
+        instance.set_json_by_key(self.key, buf)
+    }
+
+    pub fn reset(
+        &mut self,
+        instance: &mut M,
+        buf: &mut [u8],
+    ) -> Result<(), miniconf::Error<postcard::Error>>
+    where
+        M: for<'de> Postcard<'de, Y> + Default,
+    {
+        let def = M::default();
+        let mut iter = M::iter_packed();
+        iter.root(self.key)?;
+        use postcard::{de_flavors::Slice as DeSlice, ser_flavors::Slice as SerSlice};
+        for keys in iter {
+            let keys =
+                keys.map_err(|depth| miniconf::Error::Traversal(Traversal::TooLong(depth)))?;
+            let val = match def.get_postcard_by_key(keys, SerSlice::new(&mut buf[..])) {
+                Err(miniconf::Error::Traversal(Traversal::Absent(_))) => {
+                    continue;
+                }
+                ret => ret?,
+            };
+            let _rest = match instance.set_postcard_by_key(keys, DeSlice::new(val)) {
+                Err(miniconf::Error::Traversal(Traversal::Absent(_))) => {
+                    continue;
+                }
+                ret => ret?,
+            };
+        }
+        Ok(())
+    }
+
+    pub async fn dump<W>(
+        &self,
+        instance: &M,
+        mut write: W,
+        buf: &mut [u8],
+    ) -> Result<(), Error<W::Error>>
+    where
+        W: AWrite,
+        M: for<'de> JsonCoreSlash<'de, Y> + Default,
+    {
+        let def = M::default();
+        let bl = buf.len();
+        let mut sl = &mut buf[..];
+        M::path(self.key, WriteWrap(&mut sl), SEPARATOR)?;
+        let root_len = bl - sl.len();
+        awrite(&mut write, &buf[..root_len]).await?;
+        awrite(&mut write, ">\n".as_bytes()).await?;
+        let mut iter = PackedIter::<M, Y, D>::default();
+        iter.root(self.key)?;
+        for keys in iter {
+            let keys = keys?;
+            let (val, rest) = match instance.get_json_by_key(keys, &mut buf[..]) {
+                Err(miniconf::Error::Traversal(Traversal::TooShort(_))) => {
+                    ("...\n".as_bytes(), &mut buf[..])
+                }
+                Err(miniconf::Error::Traversal(Traversal::Absent(_))) => {
+                    continue;
+                }
+                ret => {
+                    let (val, rest) = buf.split_at_mut(ret?) as _;
+                    (val as _, rest)
+                }
+            };
+            let check: u32 = yafnv::fnv1a(val.iter().copied());
+            awrite(&mut write, "  ".as_bytes()).await?;
+            let rl = rest.len();
+            let mut sl = &mut rest[..];
+            M::path(keys, WriteWrap(&mut sl), SEPARATOR)?;
+            let path_len = rl - sl.len();
+            awrite(&mut write, &rest[root_len..path_len]).await?;
+            awrite(&mut write, ": ".as_bytes()).await?;
+            awrite(&mut write, val).await?;
+            let def = match def.get_json_by_key(keys, &mut buf[..]) {
+                Err(miniconf::Error::Traversal(Traversal::TooShort(_depth))) => {
+                    continue;
+                }
+                Err(miniconf::Error::Traversal(Traversal::Absent(_depth))) => "absent".as_bytes(),
+                ret => &buf[..ret?],
+            };
+            if yafnv::fnv1a::<u32, _>(def.iter().copied()) == check {
+                awrite(&mut write, " (default)\n".as_bytes()).await?;
+            } else {
+                awrite(&mut write, " (default: ".as_bytes()).await?;
+                awrite(&mut write, def).await?;
+                awrite(&mut write, ")\n".as_bytes()).await?;
+            }
+        }
+        Ok(())
+    }
 }
 
 #[cfg(all(test, feature = "std"))]
 mod tests {
     use miniconf::Tree;
-    use tokio::io;
 
     use super::*;
 
@@ -212,7 +271,7 @@ mod tests {
     }
 
     #[derive(Tree, Default)]
-    struct S {
+    struct Set {
         a: i32,
         #[tree(depth = 1)]
         b: [i32; 3],
@@ -222,32 +281,36 @@ mod tests {
         d: Inner,
         #[tree(depth = 2)]
         f: [Inner; 2],
+        #[tree(depth = 4)]
+        g: [[[Inner; 1]; 1]; 1],
     }
+    const Y: usize = 5;
 
     #[test]
     fn new() {
-        let mut s = S::default();
-        const D: usize = 16;
-        const B: usize = 1024;
-        let mut m = Menu::<S, 3, B>::default();
-        for p in m.list::<D, ()>(None).unwrap() {
+        const S: usize = 128;
+        let m = Menu::<Set, Y>::default();
+        for p in m.list::<S>().unwrap() {
             println!("{}", p.unwrap());
         }
     }
 
     #[tokio::test]
     async fn dump() {
-        const D: usize = 16;
-        const B: usize = 1024;
-        let mut buf = [0u8; B];
-        let mut s = S::default();
+        let mut buf = [0; 1024];
+        let mut s = Set::default();
         s.c = Some(8);
-        let mut m = Menu::<S, 3, 128>::default();
-        let len = m.dump::<_, D, B>(&s, None, &mut buf[..]).await.unwrap();
-        println!("{}", core::str::from_utf8(&buf[..len]).unwrap());
+        s.b[0] = 1234;
+        s.f[1].e = 9;
+        let mut stdout = embedded_io_adapters::tokio_1::FromTokio::new(tokio::io::stdout());
+        let mut m = Menu::<Set, Y, 3>::default();
+        m.dump(&s, &mut stdout, &mut buf).await.unwrap();
         m.enter("f").unwrap();
-        let len = m.dump::<_, D, B>(&s, None, &mut buf[..]).await.unwrap();
-        println!("{}", core::str::from_utf8(&buf[..len]).unwrap());
-        m.exit().unwrap();
+        m.dump(&s, &mut stdout, &mut buf).await.unwrap();
+        m.exit(1).unwrap();
+
+        m.push("c").unwrap().0.reset(&mut s, &mut buf).unwrap();
+        m.push("b").unwrap().0.reset(&mut s, &mut buf).unwrap();
+        m.dump(&s, &mut stdout, &mut buf).await.unwrap();
     }
 }
