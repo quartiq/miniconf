08ee095feb801abb46dd7015be0daf1cdd41a678
Robert Jördens <rj@quartiq.de>
2024-10-17 09:37:19 +0000

    flake: finish

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
   description = "Miniconf";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       aiomqtt22 = pkgs.python3Packages.aiomqtt.overrideAttrs rec {
         version = "2.2.0";
         src = pkgs.fetchFromGitHub {
-            owner = "sbtinstruments";
-            repo = "aiomqtt";
-            rev = "refs/tags/v${version}";
-            hash = "sha256-Sn9wGN93g61tPxuUZbGuElBXqnMEzJilfl3uvnKdIG0=";
+          owner = "sbtinstruments";
+          repo = "aiomqtt";
+          rev = "refs/tags/v${version}";
+          hash = "sha256-Sn9wGN93g61tPxuUZbGuElBXqnMEzJilfl3uvnKdIG0=";
         };
         propagatedBuildInputs = [
-            pkgs.python3Packages.paho-mqtt_2
-            pkgs.python3Packages.typing-extensions
+          pkgs.python3Packages.paho-mqtt_2
+          pkgs.python3Packages.typing-extensions
         ];
       };
       miniconf-mqtt-py = pkgs.python3Packages.buildPythonPackage {
@@ -32,14 +33,15 @@
         ];
         # checkPhase = "python -m miniconf";
       };
-    in {
+    in
+    {
       packages.x86_64-linux = {
         inherit miniconf-mqtt-py aiomqtt22;
         default = miniconf-mqtt-py;
       };
       devShells.x86_64-linux.default = pkgs.mkShellNoCC {
         name = "miniconf-dev-shell";
-        buildInputs = (pkgs.python3.withPackages(ps: with ps; [ miniconf-mqtt-py ]))
+        packages = [ miniconf-mqtt-py ];
       };
     };
 }
