6e900f9ecd959c697093acc966c854985917daa0
Robert Jördens <rj@quartiq.de>
2024-10-23 17:27:08 +0200

    fix bounds compiletest

--- miniconf/tests/ui/bounds.rs
+++ miniconf/tests/ui/bounds.rs
@@ -1,9 +1,13 @@
-use miniconf::{Metadata, Tree};
+#[allow(unused_imports)]
+use miniconf::TreeKey;
+use miniconf::{Metadata, Tree, Leaf};
 
 #[derive(Tree)]
 struct S<T>(Option<Option<T>>);
 
 fn main() {
-    // does not compile as u32 does not implement Tree
+    // This is fine:
+    S::<[Leaf<u32>; 3]>::traverse_all::<Metadata>();
+    // This does not compile as u32 does not implement TreeKey
     S::<u32>::traverse_all::<Metadata>();
 }
