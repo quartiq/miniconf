d82cdfa9737b307dfe573b1b0ddd039e256473ee
Robert Jördens <rj@quartiq.de>
2026-05-27 19:04:19 +0200

    mqtt: make example more usable

--- miniconf_mqtt/examples/miniconf.rs
+++ miniconf_mqtt/examples/miniconf.rs
@@ -1,6 +1,6 @@
 use embedded_io_adapters::tokio_1::FromTokio;
 use miniconf_mqtt::{Error, Event, Miniconf};
-use minimq::{ConfigBuilder, Error as MqttError};
+use minimq::{ConfigBuilder, ConnectEvent, Error as MqttError};
 
 #[path = "../../miniconf/examples/common.rs"]
 mod common;
@@ -10,30 +10,53 @@ const PREFIX: &str = "test/common";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn core::error::Error>> {
-    env_logger::init();
+    let _ = env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
+        .try_init();
     defmt2log::init_from_current_exe();
 
+    let broker = std::env::args().nth(1).unwrap_or_else(|| BROKER.into());
     let mut buffer = vec![0; 4096];
     let (mut miniconf, mut session) =
         Miniconf::new(PREFIX, ConfigBuilder::from_buffer(&mut buffer, 1024)?)?;
     let mut settings = common::Settings::new();
-    println!("Serving common fixture on {PREFIX}");
+    defmt::info!("serving common fixture prefix={=str}", PREFIX);
+    defmt::info!(
+        "try: miniconf --broker {=str} {=str} /control/enabled",
+        broker.as_str(),
+        PREFIX
+    );
+    defmt::info!(
+        "try: miniconf --broker {=str} {=str} /control/enabled=false",
+        broker.as_str(),
+        PREFIX
+    );
 
     loop {
-        let io = tokio::net::TcpStream::connect(BROKER).await?;
+        defmt::info!("connecting to mqtt://{=str}", broker.as_str());
+        let io = tokio::net::TcpStream::connect(&broker).await?;
         let event = session.connect(FromTokio::new(io)).await?;
         miniconf.startup(&mut session, &settings, event).await?;
-        println!("{:?}", event);
+        defmt::info!("mqtt session ready event={=str}", connect_event(event));
 
         loop {
             match miniconf.serve(&mut session, &mut settings, |_| ()).await {
                 Ok(Event::Unhandled(())) => {}
                 Ok(Event::Changed(idx)) => {
-                    println!("Settings updated: {idx:?}");
+                    defmt::info!("settings updated key={}", idx);
                 }
-                Err(Error::Mqtt(MqttError::Disconnected)) => break,
+                Err(Error::Mqtt(MqttError::Disconnected)) => {
+                    defmt::warn!("mqtt disconnected; reconnecting");
+                    break;
+                }
                 Err(err) => panic!("{err}"),
             }
         }
+    }
+}
+
+fn connect_event(event: ConnectEvent) -> &'static str {
+    match event {
+        ConnectEvent::Connected => "connected",
+        ConnectEvent::Reconnected => "reconnected",
     }
 }
